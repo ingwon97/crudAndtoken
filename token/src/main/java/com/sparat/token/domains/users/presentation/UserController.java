9e121e2a2eb78c90aaf113a3c7cad62c5003ecec
BackDev19 <ghkdrnjs180@naver.com>
2022-08-04 08:33:59 +0900

    JWT 기능 추가 (임시)

---
+++ token/src/main/java/com/sparat/token/domains/users/presentation/UserController.java
@@ -0,0 +1,39 @@
+package com.sparat.token.domains.users.presentation;
+
+import com.sparat.token.domains.users.application.UserService;
+import com.sparat.token.domains.users.application.dto.TokenResponse;
+import com.sparat.token.domains.users.application.dto.UserRequest;
+import com.sparat.token.domains.users.domain.UsersEntity;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class UserController {
+
+  private final UserService userService;
+
+  @PostMapping("/user/signUp")
+  public ResponseEntity signUp(@RequestBody UserRequest userRequest) {
+    return userService.findByUserId(userRequest.getUserId()).isPresent()
+        ? ResponseEntity.badRequest().build()
+        : ResponseEntity.ok(userService.signUp(userRequest));
+  }
+
+  @PostMapping("/user/signIn")
+  public ResponseEntity<TokenResponse> signIn(@RequestBody UserRequest userRequest) throws Exception {
+
+    return ResponseEntity.ok().body(userService.signIn(userRequest));
+  }
+
+  @GetMapping("/info")
+  public ResponseEntity<List<UsersEntity>> findUser() {
+    return ResponseEntity.ok().body(userService.findUsers());
+  }
+}
