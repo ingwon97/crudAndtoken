300fbb50bbd763bd30d8c9462dcba577cb9d383e
BackDev19 <ghkdrnjs180@naver.com>
2022-08-04 07:00:36 +0900

    오류 삭제

---
+++ sparta/token/provider/JwtProvider.java
@@ -0,0 +1,12 @@
+package com.sparta.token.provider;
+
+import lombok.RequiredArgsConstructor;
+import lombok.Value;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class JwtProvider {
+}
