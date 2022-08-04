0b8b0b81857a378c25bac32e3afeff5129f0019b
BackDev19 <ghkdrnjs180@naver.com>
2022-08-04 16:55:43 +0900

    security 처리 임시저장

--- token/src/main/java/com/sparat/token/commons/security/SpringSecurity.java
+++ token/src/main/java/com/sparat/token/commons/security/SpringSecurity.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,10 +15,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
   @Override
+  public void configure(WebSecurity web) {
+    // h2-console 사용에 대한 허용 (CSRF, FrameOptions 무시)
+    web
+            .ignoring()
+            .antMatchers("/h2-console/**");
+  }
+  @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable().cors().disable().headers().frameOptions().disable();
 
-/*    http.authorizeRequests()
+    http.csrf()
+            .ignoringAntMatchers("/user/**");
+
+    http.authorizeRequests()
             .antMatchers(HttpMethod.GET, "/api/post").permitAll()
             .antMatchers(HttpMethod.GET, "/api/post/{id}").permitAll()
             .antMatchers(HttpMethod.POST, "/user/signUp").permitAll()
@@ -25,8 +36,16 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
             .access("hasRole(ROLE_MEMBER)")
             .anyRequest().authenticated()
             .and()
-            .formLogin().disable()
-            .httpBasic().disable();*/
+            .formLogin()
+            .loginPage("/user/signIn")
+            .loginProcessingUrl("/user/signIn")
+            .permitAll()
+            .and()
+            .logout()
+            // 로그아웃 요청 처리 URL
+            .logoutUrl("/user/logout")
+            .permitAll();
+//            .httpBasic().disable();
   }
   @Bean
   public PasswordEncoder passwordEncoder(){
